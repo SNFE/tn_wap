8c53489c216af0c0b4e0f7c53b6a378008f554ba
hanaarena <hanaarena@gmail.com>
2015-02-07 18:24:25 +0800

    rebuild with amazeUI

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var $ = require('jquery');
+
+// 使用 Amaze UI 源码中的模块
+var addToHome = require('../../node_modules/amazeui/js/ui.add2home');
+
+// 使用 NPM 中的模块
+var detector = require('detector');
+
+$(function() {
+	/* $('#browser-info').append('浏览器信息：<pre>' +
+	JSON.stringify(detector.browser) +
+	'</pre>'
+	);
+
+	addToHome();*/
+
+  	/* header全局menu下拉框 */
+	$("header .menu").click(function(){
+		$("div",this).slideToggle(200)
+	});
+
+});
+
