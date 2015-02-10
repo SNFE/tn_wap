7b49160c925e5ef01b3dedfe87201acd7001611d
hanaarena <hanaarena@gmail.com>
2015-02-10 17:55:27 +0800

    add banner & data show panel

--- app/js/main.js
+++ app/js/main.js
@@ -3,22 +3,22 @@
 var $ = require('jquery');
 
 // 使用 Amaze UI 源码中的模块
-var addToHome = require('../../node_modules/amazeui/js/ui.add2home');
+//var addToHome = require('../../node_modules/amazeui/js/ui.add2home');
 
 // 使用 NPM 中的模块
-var detector = require('detector');
+//var detector = require('detector');
 
 $(function() {
-	/* $('#browser-info').append('浏览器信息：<pre>' +
+	/*$('#browser-info').append('浏览器信息：<pre>' +
 	JSON.stringify(detector.browser) +
 	'</pre>'
 	);
 
 	addToHome();*/
 
-  	/* header全局menu下拉框 */
-	$("header .menu").click(function(){
-		$("div",this).slideToggle(200)
+  	//header全局menu下拉框 
+	$('header .menu').click(function() {
+		$('div',this).slideToggle('200');
 	});
 
 });
